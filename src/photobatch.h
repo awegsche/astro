530c309c9eb301a12285f92556b456348114a69c
awegsche <a.wegscheider7141@gmail.com>
2023-01-11 17:42:04 +0100

    adding LibRaw and load raw images into batch

--- src/photobatch.h
+++ src/photobatch.h
@@ -1,14 +1,34 @@
 #ifndef PHOTOBATCH_H_
 #define PHOTOBATCH_H_
 
+#include "image.h"
 #include <filesystem>
+#include <libraw/libraw.h>
+#include <spdlog/spdlog.h>
 #include <vector>
 
 namespace fs = std::filesystem;
 
 class PhotoBatch {
   public:
-    explicit PhotoBatch(fs::path const &path) : m_root(path) {}
+    explicit PhotoBatch(fs::path const &path) : m_root(path) {
+        const auto path_lights = m_root / "lights";
+        const auto path_darks  = m_root / "darks";
+
+        LibRaw processor;
+
+        for (auto const &f : fs::directory_iterator{path_lights}) {
+            if (f.is_regular_file()) {
+                m_lights.emplace_back(f.path(), &processor);
+            }
+        }
+
+        for (auto const &f : fs::directory_iterator{path_darks}) {
+            if (f.is_regular_file()) {
+                m_darks.emplace_back(f.path(), &processor);
+            }
+        }
+    }
 
     /// Combines darks (adding up their values).
     ///
@@ -21,8 +41,8 @@ class PhotoBatch {
   private:
     fs::path m_root;
 
-    std::vector<fs::path> m_lights;
-    std::vector<fs::path> m_darks;
+    std::vector<Image> m_lights;
+    std::vector<Image> m_darks;
 };
 
 #endif // PHOTOBATCH_H_
