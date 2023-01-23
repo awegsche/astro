68626fbaf7fa7c0aa0845b0fb8ec1a7c785c7306
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2023-01-23 23:00:42 +0100

    add Eigen3 for linalg

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,82 +1,75 @@
-#include "frame.h"
+#include <algorithm>
 #include <filesystem>
 #include <image.h>
 #include <iostream>
 #include <libraw/libraw.h>
 #include <nbt.h>
+#include <spdlog/spdlog.h>
 #include <stdint.h>
+#include <vector>
+
+#include <eigen3/Eigen/Dense>
+
+#include "frame.h"
+#include "star_detection.h"
 
 using nbt::nbt_node;
 using std::cout, std::endl;
 
-template <typename Color> void to_nbt(fs::path const &path, Frame<Color> const &frame) {
-    uint32_t width  = static_cast<uint32_t>(frame.width());
-    uint32_t height = static_cast<uint32_t>(frame.height());
-    uint32_t size   = width * height * static_cast<uint32_t>(sizeof(Color));
-
-    nbt_node node{nbt::NbtTagType::TAG_Compound};
-}
-
-int main() {
+int main(int argc, char **argv) {
     cout << "Hello" << endl;
     cout << "this tool will convert raw files into a more manageable nbt format" << endl;
 
-    fs::path root = "C:/Astro/test/lights";
+    for (int arg = 0; arg < argc; ++arg)
+        cout << argv[arg] << endl;
 
-    LibRaw processor;
+    fs::path root = "K:/Astro/test/lights";
 
-    nbt_node::compound meta;
-
-    meta.insert_node(12, "width");
-    meta.insert_node(12, "height");
-    meta.insert_node(1024LL, "size");
-
-    nbt_node n = std::move(meta);
-    cout << n << endl;
+    std::vector<std::vector<Eigen::Vector2f>> all_the_stars;
 
     for (const auto &info : fs::directory_iterator{root}) {
-        cout << info.path() << endl;
 
-        processor.open_file(info.path().string().c_str());
+        spdlog::info("extension: {}", info.path().extension().string());
+        if (info.path().extension() != ".nbt")
+            continue;
 
-        cout << "unpacking raw" << endl;
-        processor.unpack();
+        spdlog::info("loading nbt image {}", info.path().string());
+        BayerImage im{info.path()};
+        // im.reload();
+        //  this is for importing and converting
+        //  auto im = BayerImage::import_raw(info.path());
 
-        cout << "raw -> image" << endl;
-        processor.raw2image();
+        // detect stars
+        // im.detect_stars();
+        //
+        //
+        im.load_meta();
 
-        Frame<Cu16> reds;
-        Frame<Cu16> blues;
-        Frame<Cu16> greens_1;
-        Frame<Cu16> greens_2;
+        auto &detected_stars = all_the_stars.emplace_back();
+        std::transform(im.reds().detected_stars.cbegin(), im.reds().detected_stars.cbegin() + 30,
+                       std::back_inserter(detected_stars), [](pixel_value<float> const &s) -> Eigen::Vector2f {
+                           return {s.x, s.y};
+                       });
+    }
 
-        auto width        = processor.imgdata.sizes.width / 2;
-        auto height       = processor.imgdata.sizes.height / 2;
-        auto const &image = processor.imgdata.image;
+    constexpr int WINDOW = 5;
 
-        reds.resize(width, height);
-        blues.resize(width, height);
-        greens_1.resize(width, height);
-        greens_2.resize(width, height);
+    std::array<float, WINDOW> distances;
 
-        const auto get_index = [width, height](int x, int y) { return y * width + x; };
+    for (int i = 0; i < 10; ++i) {
+        auto reference = all_the_stars[0][i];
+        spdlog::info("reference: ({}, {})", reference.x(), reference.y());
 
-        cout << "converting raw data" << endl;
+        int offset = std::max(0, i - WINDOW / 2);
 
-        for (int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                greens_1.at_mut(x, y).ch = image[get_index(2 * x, 2 * y)][1];
-                blues.at_mut(x, y).ch    = image[get_index(2 * x + 1, 2 * y)][2];
-                reds.at_mut(x, y).ch     = image[get_index(2 * x, 2 * y + 1)][0];
-                greens_2.at_mut(x, y).ch = image[get_index(2 * x + 1, 2 * y + 1)][3];
-            }
+        for (int j = 0; j < WINDOW; ++j) {
+            distances[j] = (all_the_stars[1][offset + j] - reference).norm();
         }
 
-        cout << " done " << endl;
+        auto idx = std::distance(distances.cbegin(), std::min_element(distances.cbegin(), distances.cend()));
 
-        for (int i = 0; i < 10; ++i) {
-            cout << greens_1.data()[i].ch << endl;
-        }
+        auto const &closest = all_the_stars[1][offset + idx];
+        spdlog::info("closest to reference: {} | ({}, {})", distances[idx], closest.x(), closest.y());
     }
 
     return 0;
